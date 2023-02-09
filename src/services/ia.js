8df0581ad9fe8a15f7ccd8c3d59524ed9dd161c9
DavidCs9 <davidcastrosi@hotmail.com>
2023-02-09 02:29:32 -0600

    delete comments in ia.js

--- src/services/ia.js
+++ src/services/ia.js
@@ -1,26 +1,6 @@
 const COHERE_API_KEY = 'B94nbu2xiHwXSaL9UdEwm6wOggpM3ypBjwSABvU5'
 const COHERE_API_GENERATE_URL = 'https://api.cohere.ai/generate'
 
-/*
-curl --location --request POST 'https://api.cohere.ai/generate' \
-  --header 'Authorization: BEARER B94nbu2xiHwXSaL9UdEwm6wOggpM3ypBjwSABvU5' \
-  --header 'Content-Type: application/json' \
-  --header 'Cohere-Version: 2022-12-06' \
-  --data-raw '{
-      "model": "command-xlarge-nightly",
-      "prompt": "This is a movie recommender. Please make a list of four movies. My mood is fun",
-      "max_tokens": 300,
-      "temperature": 0.9,
-      "k": 0,
-      "p": 0.75,
-      "frequency_penalty": 0,
-      "presence_penalty": 0,
-      "stop_sequences": [],
-      "return_likelihoods": "NONE"
-    }'
-    */
-
-
 
 export async function recommendMovies(moodInput) {
     const data = {
