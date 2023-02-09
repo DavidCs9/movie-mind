52b3183fa9862496d3cda6f0259b72d4701b48b3
DavidCs9 <davidcastrosi@hotmail.com>
2023-02-09 13:30:53 -0600

    add env var

--- src/services/ia.js
+++ src/services/ia.js
@@ -1,12 +1,17 @@
+import * as dotenv from 'dotenv'
+dotenv.config()
+
+
 const API_KEY =  process.env.COHERE_API_KEY
 const COHERE_API_GENERATE_URL = 'https://api.cohere.ai/generate'
 
-console.log(COHERE_API_KEY)
+console.log(API_KEY)
 
 export async function recommendMovies(moodInput) {
     const data = {
         model: 'command-xlarge-nightly',
-        prompt: `This is a movie recommender based in a mood. Tell me about three movies describing each in a paragraph, my mood is: ${moodInput}`,
+        prompt: `This is a movie recommender based in a mood. Tell me about three movies describing 
+        each in a paragraph, my mood is: ${moodInput}`,
         max_tokens: 400,
         temperature: 1.5,
         k: 0,
